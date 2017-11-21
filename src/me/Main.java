42b870590b782d54c6d0f8ce3fbb1b4b6cc8bdeb
Michal <238181@student.pwr.edu.pl>
2017-11-21 07:57:07 +0100

    Initial commit.

---
+++ src/me/Main.java
@@ -0,0 +1,11 @@
+package me;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String article = JOptionPane.showInputDialog(null, "Please, input fragment to summarize.");
+    }
+
+}
